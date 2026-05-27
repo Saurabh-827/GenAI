da0d954124cf0337e0c28632cfadd53249e5e508
Saurabh-827 <saurabhrabta@gmail.com>
2026-05-28 00:54:25 +0530

    work with packages and subpackages

---
+++ 05_Modules/package/__init__.py
@@ -0,0 +1,2 @@
+'''__init__.py is used to mark a directory as a Python package. 
+It can be an empty file or contain initialization code for the package.'''
---
+++ 05_Modules/test.py
@@ -0,0 +1,8 @@
+from package.maths import *
+from package.subpackage.div import divison
+
+print(addition(10, 20))
+print(substraction(10, 20))
+print(multiplication(10, 20))
+
+print(divison(90,10))
